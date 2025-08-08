496a9ca0ed467c6fb64da089db653f1eec422c8f
Lawal Daniel <lawaldaniel34@gmail>
2025-08-08 17:07:58 +0100

    fast_api

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,12 +1,16 @@
 from jose import JWTError,jwt
 from datetime import datetime, timedelta
 from . import schemas
+from fastapi import Depends, status , HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 #SECRET_KEY
 #Algorithm
 #Expiration time
 
-#SECRET_KEY = 
-#ALGORITHM = "HS256"
+SECRET_KEY = "056928975wofhwhdjlhduiqidhlfhddblqdq8900834r4"
+ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def create_access_token(data: dict):
@@ -29,5 +33,7 @@ def verify_access_token(token: str,credentials_exception):
         raise credentials_exception
     
     
-def get_current_user():
-    pass
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail=f"Could not validate credentials",
+    headers= {"WWW-Authenticate":"Bearer"})
+    return verify_access_token(token,credentials_exception)
